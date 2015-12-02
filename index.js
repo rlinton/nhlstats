4438b7e25db5453a205057bb73be38e0409dbda4
rlinton <rlinton@gmail.com>
2015-12-01 23:50:15 -0800

    changing to lambda function approach

--- index.js
+++ index.js
@@ -4,33 +4,17 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+//Load the request module
+var request = require('request');
+var parser = require('xml2json');
+//Lets try to make a HTTP GET request to modulus.io's website.
+request('http://app.cgy.nhl.yinzcam.com/V2/Stats/Standings', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var xml = body;
+        console.log(xml);
+        var json = parser.toJson(xml); //returns a string containing the JSON structure by default
+        //console.log(json);
+        var obj = JSON.parse(json);
+    }
 });
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
